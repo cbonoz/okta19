a314d08c8fbf74bdcfdac517a127b9c2893c00f0
cbonoz <chrisdistrict@gmail.com>
2019-03-30 14:03:36 -0400

    add form

--- onboardsms/src/components/Protected.js
+++ onboardsms/src/components/Protected.js
@@ -4,15 +4,39 @@ import { Row, Col, ListGroup, ListGroupItem, Button } from 'react-bootstrap'
 import Header from './Header'
 import NotificationSystem from 'react-notification-system'
 import { parse, format } from 'date-fns'
+import Form from "react-jsonschema-form";
+
 import api from '../api'
+
+const schema = {
+  title: "New Guide",
+  type: "object",
+  required: ["name", "description", "steps"],
+  properties: {
+    name: { type: "string", title: "Guide Name", default: "CPR" },
+    description: { type: "string", title: "Description", default: "How to do CPR" },
+    steps: {
+      "type": "array",
+      "title": "Enter Steps",
+      "items": {
+        "type": "string",
+        "default": "Enter your step here!"
+      }
+    },
+    
+  }
+};
+
+
 
 class Protected extends Component {
 
-  state = { 
-    authenticated: null, 
-    currentUser: null, 
-    currentGuide: null,
-    guides: [] 
+  state = {
+    authenticated: null,
+    currentUser: null,
+    currentGuide: {},
+    guides: [],
+    loaded: false
   }
 
   constructor(props) {
@@ -64,33 +88,36 @@ class Protected extends Component {
     this.checkAuthentication(true)
   }
 
-  async createGuide(guide) {
-    try {
-      const createdGuide = await api.createGuide(guide)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
   async getGuidesForUser(currentUser) {
     try {
       const author = currentUser.name
-      const guides = await api.getGuidesByAuthor(author)
-      this.setState({ guides })
+      const response = await api.getGuidesByAuthor(author)
+      const guides = response.data
+      console.log('myGuides', guides)
+      this.setState({ guides, loaded: true })
     } catch (e) {
       console.error(e)
     }
   }
 
   clearGuide() {
-    this.setState({currentGuide: null})
+    this.setState({ currentGuide: {} })
   }
 
-  submitGuide() {
+  async submitGuide() {
     const { currentGuide } = this.state
-    const createdGuide = this.createGuide(currentGuide)
+    let response
+    try {
+      response = await api.createGuide(currentGuide)
+    } catch (e) {
+      console.error('error creating guide', e)
+      const errorMessage = `Couldn't create guide: ${e.response.data}`
+      this.createNotification(errorMessage, 'error')
+      return
+    }
+    const createdGuide = response.data
     if (createdGuide) {
-      this.createNotification('Created Guide', 'success')
+      this.createNotification('Created Guide: ' + createdGuide.name, 'success')
       this.clearGuide()
     }
   }
@@ -100,42 +127,50 @@ class Protected extends Component {
   }
 
   render() {
-    const { authenticated, guides } = this.state
+    const { authenticated, guides, loaded } = this.state
     const { auth } = this.props
+    const log = (type) => console.log.bind(console, type);
+
+    const onSubmit = ({ formData }, e) => {
+      console.log("Data submitted: ", formData);
+      this.setState({ currentGuide: formData })
+    }
+
     return (
       <div>
         <Header authenticated={authenticated} auth={auth} login={() => this.login()} logout={() => this.logout()} />
         <Row className='create-section gradient-background'>
           {/* Existing Guide Section */}
           <Col xs={12} md={6}>
-            {/* <div className='centered'>
-              <h2 className='centered create-header'>My Guides</h2>
-            </div> */}
 
-            <ListGroupItem>Header</ListGroupItem>
-            <ListGroupItem>
-
-            {guides.map((guide) => {
-              const formattedDate = format(parse(guide.createdAt * 1000), 'MM/dd/yyyy')
-              return <div className='guide-item'>
-                <div onClick={(g) => this.deleteGuide(g)}></div>
-                <h3>Name: {guide.name}</h3>
-                <p>Created: {formattedDate}</p>
+            <ListGroupItem variant="light">Your Guides</ListGroupItem>
+            {loaded && <ListGroupItem>
+              {(!guides || guides.length == 0) && <div>
+                <h5><b>Any guides you create will be visible here!</b></h5>
               </div>
-            })}
+              }
+              {guides.map((guide, i) => {
+                const formattedDate = format(parse(guide.createdAt * 1000), 'MM/dd/yyyy')
+                return <div className='guide-item' key={i}>
+                  <div onClick={(g) => this.deleteGuide(g)}></div>
+                  <h3>Name: {guide.name}</h3>
+                  <p>Created: {formattedDate}</p>
+                </div>
+              })}
+
             </ListGroupItem>
+            }
           </Col>
 
           {/* Create Guide Section */}
           <Col xs={12} md={6}>
-            {/* <div className='centered'>
-              <h2 className='centered create-header'>Create Guide</h2>
-            </div> */}
 
-            <ListGroupItem bsStyle='info'>Header</ListGroupItem>
+            <ListGroupItem variant="success">Create New Guide</ListGroupItem>
             <ListGroupItem>
-              <Button onClick={() => this.submitGuide()} bsStyle='success'>Create</Button>
-              <Button onClick={() => this.clearGuide()} bsStyle='danger'>Cancel</Button>
+              <Form schema={schema}
+                onChange={log("changed")}
+                onSubmit={onSubmit}
+                onError={log("errors")} />
             </ListGroupItem>
 
           </Col>
